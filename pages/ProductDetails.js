a236c2d391b820a3c896fb5e700df80f977b701f
matsegard <matsegard@outlook.com>
2022-12-20 22:49:52 +0100

    added image slider

--- pages/ProductDetails.js
+++ pages/ProductDetails.js
@@ -7,6 +7,7 @@ import {
   Alert,
   Pressable,
   ScrollView,
+  Dimensions,
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
@@ -14,6 +15,7 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
 import { faWeightHanging } from "@fortawesome/free-solid-svg-icons";
 import { faBarsProgress } from "@fortawesome/free-solid-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const product = {
   title: "Taylormade M4",
@@ -29,14 +31,59 @@ const product = {
 };
 
 export default function ProductDetails() {
+  const images = [
+    "https://plus.unsplash.com/premium_photo-1661774316407-56209baefa8c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1535131749006-b7f58c99034b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={require("../assets/test.jpg")} />
+        <ScrollView
+          pagingEnabeld
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          decelerationRate={0}
+          snapToInterval={400}
+          snapToAlignment={"center"}
+        >
+          {images.map((image, index) => (
+            <Image key={index} style={styles.image} source={{ uri: image }} />
+          ))}
+        </ScrollView>
+        <View>
+          <FontAwesomeIcon
+            style={{
+              flexDirection: "row",
+              position: "absolute",
+              color: "blue",
+              bottom: 28,
+            }}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={{
+              flexDirection: "row",
+              position: "absolute",
+              color: "blue",
+              bottom: 28,
+            }}
+            icon={faCircle}
+          />
+          <FontAwesomeIcon
+            style={{
+              flexDirection: "row",
+              position: "absolute",
+              color: "blue",
+              bottom: 28,
+            }}
+            icon={faCircle}
+          />
+        </View>
       </View>
 
       <View style={styles.infoContainer}>
-        <View style={styles.TopInfoContainer}>
+        <View style={styles.topInfoContainer}>
           <Text style={styles.titleText}>{product.title}</Text>
           <View style={styles.location}>
             <FontAwesomeIcon style={styles.locationIcon} icon={faLocationDot} />
@@ -126,7 +173,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   infoContainer: {
-    flex: 2.5,
+    flex: 2,
     backgroundColor: "white",
     borderTopLeftRadius: "20%",
     borderTopRightRadius: "20%",
@@ -134,20 +181,23 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     padding: "4%",
   },
-  TopInfoContainer: {
+  topInfoContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     height: "10%",
+    marginBottom: 8,
   },
   imageContainer: {
-    flex: 2,
+    height: "40%",
+    marginBottom: -20,
     justifyContent: "center",
     alignItems: "center",
   },
   image: {
-    resizeMode: "contain",
-    width: "100%",
+    resizeMode: "cover",
+    width: 400,
+    height: "100%",
   },
   location: {
     flexDirection: "row",
