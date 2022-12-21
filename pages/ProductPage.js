3caacba31310acbcf09cbc5a84436dbe608022a9
Max Andersson <maxbjornkga@gmail.com>
2022-12-21 15:05:32 +0100

    s

--- pages/ProductPage.js
+++ pages/ProductPage.js
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View, Button } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faMugSaucer } from "@fortawesome/free-solid-svg-icons/faMugSaucer";
 import Navbar from "../components/Navbar"
-import ProductCard from "../components/product/productCard";
-import CategoriBar from "../components/product/categoriBar";
-import SearchBar from "../components/product/searchBar";
+import ProductCard from "../components/productCard";
+import CategoriBar from "../components/categoriBar";
+import SearchBar from "../components/searchBar";
 export default function ProductPage({ navigation }) {
   return (
     <View style={styles.container}>
