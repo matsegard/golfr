a0f9d0bf19adb3c3e3ec2b3fd4c95d4f036ba9c8
matsegard <matsegard@outlook.com>
2022-12-21 14:06:15 +0100

    datepicker

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,44 +1,78 @@
-export const DatePicker = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { Text, View, Button, TextInput } from "react-native";
+import React, { useState } from "react";
+
+function useInput() {
+  const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
-  };
-
   const showMode = (currentMode) => {
-    if (Platform.OS === "android") {
-      setShow(false);
-      // for iOS, add a button that closes the picker
-    }
+    setShow(true);
     setMode(currentMode);
   };
-
   const showDatepicker = () => {
     showMode("date");
   };
 
-  const showTimepicker = () => {
-    showMode("time");
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+    setShow(false);
+    console.log(selectedDate);
   };
+  return {
+    date,
+    showDatepicker,
+    show,
+    mode,
+    onChange,
+  };
+}
 
+export default function DatePicker() {
+  const input = useInput(new Date());
+  const input2 = useInput(new Date());
   return (
-    <View>
-      <Button onPress={showDatepicker} title="Show date picker!" />
-      <Button onPress={showTimepicker} title="Show time picker!" />
-      <Text>selected: {date.toLocaleString()}</Text>
-      {show && (
+    <View
+      style={{
+        flexDirection: "column",
+        justifyContent: "center",
+        // alignItems: "center",
+        flex: "1",
+      }}
+    >
+      <Button
+        onPress={input.showDatepicker}
+        title={input.date.toLocaleDateString()}
+      />
+      {input.show && (
         <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode={mode}
+          testID="dateTimePicker1"
+          value={input.date}
+          mode={input.mode}
           is24Hour={true}
-          onChange={onChange}
+          display="default"
+          onChange={input.onChange}
         />
       )}
+
+      <Button
+        onPress={input2.showDatepicker}
+        title={input2.date.toLocaleDateString()}
+      />
+      {input2.show && (
+        <DateTimePicker
+          testID="dateTimePicker2"
+          value={input2.date}
+          mode={input2.mode}
+          is24Hour={true}
+          display="default"
+          onChange={input2.onChange}
+        />
+      )}
+      <Text></Text>
     </View>
   );
-};
+}
