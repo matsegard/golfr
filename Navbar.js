ca6266ea264a61be146d4bd68e4013e8b8f44dee
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-12-21 09:59:17 +0100

    navbar logic

--- Navbar.js
+++ Navbar.js
@@ -8,95 +8,100 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyin
 import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 
 export default function Navbar() {
-  const [selected, setSelected] = useState(false);
-
-  let icons = [faHouse, faBell, faSquarePlus, faMagnifyingGlass, faUser];
-
-  const handlePress = (icon) => () =>
-    setSelected(selected => ({
-      ...icon,
-      [icon]: !selected[icon],
-    }));
+  const [selected, setSelected] = useState(0);
 
   return (
     <View style={styles.container}>
-      {icons.map((icon, i) => {
-        return (
-          <Pressable
-            key={i}
-            onPress={handlePress(icon)}
-            style={{
-              backgroundColor: selected ? "#6A8E4E" : "transparent",
-              color: selected ? "white" : "#828282",
-              height: 45,
-              width: 45,
-              alignItems: "center",
-              justifyContent: "center",
-              borderRadius: "7px",
-            }}
-          >
-            <FontAwesomeIcon
-              size={30}
-              color={selected ? "white" : "#828282"}
-              style={styles.icon}
-              icon={icon}
-            />
-          </Pressable>
-        );
-      })}
-
-      {/* <Pressable
+      <Pressable
+        onPress={() => setSelected(0)}
         style={{
-          backgroundColor: selected ? "#6A8E4E" : "transparent",
-          color: selected ? "white" : "#828282",
+          backgroundColor: selected === 0 ? "#6A8E4E" : "transparent",
+          color: selected === 0 ? "white" : "#828282",
+          height: 45,
+          width: 45,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "7px",
         }}
-        onPress={() => setSelected(!selected)}
       >
         <FontAwesomeIcon
           size={30}
-          color={selected ? "white" : "#828282"}
+          color={selected === 0 ? "white" : "#828282"}
+          style={styles.icon}
+          icon={faHouse}
+        />
+      </Pressable>
+      <Pressable
+        onPress={() => setSelected(1)}
+        style={{
+          backgroundColor: selected === 1 ? "#6A8E4E" : "transparent",
+          color: selected === 1 ? "white" : "#828282",
+          height: 45,
+          width: 45,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "7px",
+        }}
+      >
+        <FontAwesomeIcon
+          size={30}
+          color={selected === 1 ? "white" : "#828282"}
           icon={faBell}
         />
       </Pressable>
       <Pressable
+        onPress={() => setSelected(2)}
         style={{
-          backgroundColor: selected ? "#6A8E4E" : "transparent",
-          color: selected ? "white" : "#828282",
+          backgroundColor: selected === 2 ? "#6A8E4E" : "transparent",
+          color: selected === 2 ? "white" : "#828282",
+          height: 45,
+          width: 45,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "7px",
         }}
-        onPress={() => setSelected(!selected)}
       >
         <FontAwesomeIcon
           size={30}
-          color={selected ? "white" : "#828282"}
+          color={selected === 2 ? "white" : "#828282"}
           icon={faSquarePlus}
         />
       </Pressable>
       <Pressable
+        onPress={() => setSelected(3)}
         style={{
-          backgroundColor: selected ? "#6A8E4E" : "transparent",
-          color: selected ? "white" : "#828282",
+          backgroundColor: selected === 3 ? "#6A8E4E" : "transparent",
+          color: selected === 3 ? "white" : "#828282",
+          height: 45,
+          width: 45,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "7px",
         }}
-        onPress={() => setSelected(!selected)}
       >
         <FontAwesomeIcon
           size={30}
-          color={selected ? "white" : "#828282"}
+          color={selected === 3 ? "white" : "#828282"}
           icon={faMagnifyingGlass}
         />
       </Pressable>
       <Pressable
+        onPress={() => setSelected(4)}
         style={{
-          backgroundColor: selected ? "#6A8E4E" : "transparent",
-          color: selected ? "white" : "#828282",
+          backgroundColor: selected === 4 ? "#6A8E4E" : "transparent",
+          height: 45,
+          width: 45,
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "7px",
         }}
-        onPress={() => setSelected(!selected)}
       >
         <FontAwesomeIcon
           size={30}
-          color={selected ? "white" : "#828282"}
+          color={selected === 4 ? "white" : "#828282"}
           icon={faUser}
         />
-      </Pressable> */}
+      </Pressable>
     </View>
   );
 }
@@ -114,10 +119,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "white",
   },
-  navbarItem: {},
   pressable: {
     width: 5,
     height: 5,
   },
-  icon: {},
 });
