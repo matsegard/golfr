544fada152e8110936068a3ca2843afaaa1af757
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-12-24 10:38:46 +0100

    some small design fixes in categoryBar

--- components/categoriBar.js
+++ components/categoriBar.js
@@ -1,28 +1,55 @@
-import { View, Pressable, Text } from "native-base"
+import { View, Pressable, Text } from "native-base";
+import { StyleSheet } from "react-native";
 
 const CategoriBar = () => {
-    return (
-        <View  flexDirection="row" justifyContent="space-between" width="90%" margin="5" >
-            <Pressable  onPress={() => console.log("Klubbor kategori")}>
-              <Text fontSize="xl"  >Klubbor</Text>    
-            </Pressable> 
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => console.log("Klubbor kategori")}>
+        <Text style={styles.categoryText} fontSize="xl">
+          Klubbor
+        </Text>
+      </Pressable>
 
-            <Pressable onPress={() => console.log("Golfset kategori")}>
-              <Text fontSize="xl" >Golfset</Text>    
-            </Pressable> 
+      <Pressable onPress={() => console.log("Golfset kategori")}>
+        <Text style={styles.categoryText} fontSize="xl">
+          Golfset
+        </Text>
+      </Pressable>
 
-            <Pressable onPress={() => console.log("Golfset Vagn")} >
-              <Text fontSize="xl" >Vagn</Text>    
-            </Pressable>
+      <Pressable onPress={() => console.log("Golfset Vagn")}>
+        <Text style={styles.categoryText} fontSize="xl">
+          Vagn
+        </Text>
+      </Pressable>
 
-            <Pressable onPress={() => console.log("Golfset Golfbil")} >
-              <Text fontSize="xl" >Golfbil</Text>    
-            </Pressable> 
+      <Pressable onPress={() => console.log("Golfset Golfbil")}>
+        <Text style={styles.categoryText} fontSize="xl">
+          Golfbil
+        </Text>
+      </Pressable>
 
-            <Pressable onPress={() => console.log("Golfset Övrigt")} >
-              <Text fontSize="xl" >Övrigt</Text>    
-            </Pressable> 
-        </View>
-    )
-}
+      <Pressable onPress={() => console.log("Golfset Övrigt")}>
+        <Text style={styles.categoryText} fontSize="xl">
+          Övrigt
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "85%",
+    margin: 5,
+  },
+  categoryText: {
+    fontFamily: "MontserratSemiBold",
+    fontSize: 13,
+    color: "#9B9B9B",
+    marginBottom: 15,
+  },
+});
+
 export default CategoriBar;
