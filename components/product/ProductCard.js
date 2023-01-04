af7ea4391a8cd9e441c2a60d85f55eb9350a2a2b
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-04 14:03:38 +0100

    fixed loading

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -8,7 +8,6 @@ import {
   Text,
   Center,
   Stack,
-  Button,
 } from "native-base";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
@@ -16,9 +15,12 @@ import { Pressable, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig";
+import reactDom from "react-dom";
 
 function ProductCard() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigation = useNavigation();
 
   async function getData() {
     const productsData = [];
@@ -27,108 +29,121 @@ function ProductCard() {
       // doc.data() is never undefined for query doc snapshots
       productsData.push(doc.data());
     });
-    return setProducts(productsData);
+    setProducts(productsData);
+    setLoading(false);
+    return;
   }
 
   useEffect(() => {
     getData();
   }, []);
 
-  const navigation = useNavigation();
-
   return (
     <ScrollView showsVerticalScrollIndicator={false} height="auto">
       <Box alignItems="center" marginBottom="240">
-        {products.map((item, i) => (
-          <Pressable
-            key={i}
-            onPress={() => {
-              navigation.navigate("ProductDetails", {
-                title: item.title,
-                price: item.price,
-                description: item.description,
-                location: item.location,
-                clubs: item.clubs,
-                hcp: item.hcp,
-                stiffness: item.stiffness,
-                leftRight: item.leftRight,
-                gender: item.gender,
-              });
-            }}
-          >
-            <Box
-              maxW="80"
-              rounded="lg"
-              marginBottom="5"
-              overflow="hidden"
-              borderColor="coolGray.200"
-              borderWidth="1"
-              _dark={{
-                borderColor: "coolGray.600",
-                backgroundColor: "gray.700",
-              }}
-              _web={{
-                shadow: 2,
-                borderWidth: 0,
-              }}
-              _light={{
-                backgroundColor: "gray.50",
+        {!loading ? (
+          products.map((item, i) => (
+            <Pressable
+              key={i}
+              onPress={() => {
+                navigation.navigate("ProductDetails", {
+                  title: item.title,
+                  price: item.price,
+                  description: item.description,
+                  location: item.location,
+                  clubs: item.clubs,
+                  hcp: item.hcp,
+                  stiffness: item.stiffness,
+                  leftRight: item.leftRight,
+                  gender: item.gender,
+                });
               }}
             >
-              <Box>
-                <AspectRatio w="100%" ratio={16 / 9}>
-                  <Image
-                    source={{
-                      uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
-                    }}
-                    alt="image"
-                  />
-                </AspectRatio>
-                <Center
-                  bg="#6A994E"
-                  _dark={{
-                    bg: "tertiary.500",
-                  }}
-                  _text={{
-                    color: "warmGray.50",
-                    fontWeight: "700",
-                    fontSize: "xs",
-                  }}
-                  position="absolute"
-                  bottom="0"
-                  px="3"
-                  py="1.5"
-                >
-                  {item.price} kr/dag
-                </Center>
-              </Box>
-              <Stack p="4" space={3}>
-                <Stack space={2}>
-                  <Heading size="md" ml="-1">
-                    {item.title}
-                  </Heading>
-                  <Text
-                    fontSize="xs"
-                    _light={{
-                      color: "#6A994E",
-                    }}
+              <Box
+                maxW="80"
+                rounded="lg"
+                marginBottom="5"
+                overflow="hidden"
+                borderColor="coolGray.200"
+                borderWidth="1"
+                _dark={{
+                  borderColor: "coolGray.600",
+                  backgroundColor: "gray.700",
+                }}
+                _web={{
+                  shadow: 2,
+                  borderWidth: 0,
+                }}
+                _light={{
+                  backgroundColor: "gray.50",
+                }}
+              >
+                <Box>
+                  <AspectRatio w="100%" ratio={16 / 9}>
+                    <Image
+                      source={{
+                        uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
+                      }}
+                      alt="image"
+                    />
+                  </AspectRatio>
+                  <Center
+                    bg="#6A994E"
                     _dark={{
-                      color: "tertiary.500",
+                      bg: "tertiary.500",
                     }}
-                    fontWeight="500"
-                    ml="-0.5"
-                    mt="-1"
-                    mr="5"
+                    _text={{
+                      color: "warmGray.50",
+                      fontWeight: "700",
+                      fontSize: "xs",
+                    }}
+                    position="absolute"
+                    bottom="0"
+                    px="3"
+                    py="1.5"
                   >
-                    {item.location}{" "}
-                    <FontAwesomeIcon color="#B6B6B6" icon={faLocationDot} />
-                  </Text>
+                    {item.price} kr/dag
+                  </Center>
+                </Box>
+                <Stack p="4" space={3}>
+                  <Stack space={2}>
+                    <Heading size="md" ml="-1">
+                      {item.title}
+                    </Heading>
+                    <Text
+                      fontSize="xs"
+                      _light={{
+                        color: "#6A994E",
+                      }}
+                      _dark={{
+                        color: "tertiary.500",
+                      }}
+                      fontWeight="500"
+                      ml="-0.5"
+                      mt="-1"
+                      mr="5"
+                    >
+                      {item.location}{" "}
+                      <FontAwesomeIcon color="#B6B6B6" icon={faLocationDot} />
+                    </Text>
+                  </Stack>
+                  <Text fontWeight="400">{item.description}</Text>
                 </Stack>
-                <Text fontWeight="400">{item.description}</Text>
-              </Stack>
-            </Box>
-          </Pressable>
-        ))}
+              </Box>
+            </Pressable>
+          ))
+        ) : (
+          <Box
+            sx={{
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Text fontSize="2xl" style={styles.loading}>
+              Loading...
+            </Text>
+          </Box>
+        )}
       </Box>
     </ScrollView>
   );
@@ -137,6 +152,10 @@ function ProductCard() {
 const styles = StyleSheet.create({
   margin: {
     marginTop: 20,
+  },
+  loading: {
+    margin: "auto",
+    marginTop: 50,
   },
 });
 
