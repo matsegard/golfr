0716fe6efefe6b365e67a7ebc9791a76db8685b9
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-06 13:00:55 +0100

    shows all products when loading page (image bug)

--- components/product/ProductCard.js
+++ components/product/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   ScrollView,
   Box,
@@ -21,6 +21,7 @@ function ProductCard({ selectedCategory }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+  const filteredList = useMemo(getFilteredList, [selectedCategory, products]);
 
   async function getData() {
     const productsData = [];
@@ -34,110 +35,114 @@ function ProductCard({ selectedCategory }) {
     return;
   }
 
+  function getFilteredList() {
+    if (!selectedCategory) {
+      return products;
+    }
+    return products.filter((item) => item.category === selectedCategory);
+  }
+
   useEffect(() => {
     getData();
   }, []);
-
 
   return (
     <ScrollView showsVerticalScrollIndicator={false} height="auto">
       <Box alignItems="center" marginBottom="240">
         {!loading ? (
-          products
-            .filter((p) => p.category === selectedCategory)
-            .map((item, i) => (
-              <Pressable
-                key={i}
-                onPress={() => {
-                  navigation.navigate("ProductDetails", {
-                    title: item.title,
-                    image: item.image,
-                    price: item.price,
-                    description: item.description,
-                    location: item.location,
-                    clubs: item.clubs,
-                    difficulty: item.difficulty,
-                    shaft: item.shaft,
-                    hand: item.hand,
-                    gender: item.gender,
-                    user: item.user,
-                  });
+          filteredList.map((item, i) => (
+            <Pressable
+              key={i}
+              onPress={() => {
+                navigation.navigate("ProductDetails", {
+                  title: item.title,
+                  image: item.image,
+                  price: item.price,
+                  description: item.description,
+                  location: item.location,
+                  clubs: item.clubs,
+                  difficulty: item.difficulty,
+                  shaft: item.shaft,
+                  hand: item.hand,
+                  gender: item.gender,
+                  user: item.user,
+                });
+              }}
+            >
+              <Box
+                maxW="80"
+                rounded="lg"
+                marginBottom="5"
+                overflow="hidden"
+                borderColor="coolGray.200"
+                borderWidth="1"
+                _dark={{
+                  borderColor: "coolGray.600",
+                  backgroundColor: "gray.700",
+                }}
+                _web={{
+                  shadow: 2,
+                  borderWidth: 0,
+                }}
+                _light={{
+                  backgroundColor: "gray.50",
                 }}
               >
-                <Box
-                  maxW="80"
-                  rounded="lg"
-                  marginBottom="5"
-                  overflow="hidden"
-                  borderColor="coolGray.200"
-                  borderWidth="1"
-                  _dark={{
-                    borderColor: "coolGray.600",
-                    backgroundColor: "gray.700",
-                  }}
-                  _web={{
-                    shadow: 2,
-                    borderWidth: 0,
-                  }}
-                  _light={{
-                    backgroundColor: "gray.50",
-                  }}
-                >
-                  <Box>
-                    <AspectRatio w="100%" ratio={16 / 9}>
-                      <Image
-                        source={{
-                          uri: item.image,
-                        }}
-                        alt="image"
-                      />
-                    </AspectRatio>
-                    <Center
-                      bg="#6A994E"
-                      _dark={{
-                        bg: "tertiary.500",
+                <Box>
+                  <AspectRatio w="100%" ratio={16 / 9}>
+                    <Image
+                      source={{
+                        uri: item.image,
                       }}
-                      _text={{
-                        color: "warmGray.50",
-                        fontWeight: "700",
-                        fontSize: "xs",
-                      }}
-                      position="absolute"
-                      bottom="0"
-                      px="3"
-                      py="1.5"
-                    >
-                      {item.price} kr/dag
-                    </Center>
-                  </Box>
-                  <Stack p="4" space={3}>
-                    <Stack space={2}>
-                      <Heading size="md" ml="-1">
-                        {item.title}
-                      </Heading>
-                      <Text
-                        fontSize="xs"
-                        _light={{
-                          color: "#6A994E",
-                        }}
-                        _dark={{
-                          color: "tertiary.500",
-                        }}
-                        fontWeight="500"
-                        ml="-0.5"
-                        mt="-1"
-                        mr="5"
-                      >
-                        {item.location}{" "}
-                        <FontAwesomeIcon color="#B6B6B6" icon={faLocationDot} />
-                      </Text>
-                      <Text fontSize="xs">{item.user}</Text>
-                    </Stack>
-                    <Text fontWeight="400">{item.description}</Text>
-                  </Stack>
+                      alt={item.title}
+                    />
+                  </AspectRatio>
+                  <Center
+                    bg="#6A994E"
+                    _dark={{
+                      bg: "tertiary.500",
+                    }}
+                    _text={{
+                      color: "warmGray.50",
+                      fontWeight: "700",
+                      fontSize: "xs",
+                    }}
+                    position="absolute"
+                    bottom="0"
+                    px="3"
+                    py="1.5"
+                  >
+                    {item.price} kr/dag
+                  </Center>
                 </Box>
-              </Pressable>
-            ))
+                <Stack p="4" space={3}>
+                  <Stack space={2}>
+                    <Heading size="md" ml="-1">
+                      {item.title}
+                    </Heading>
+                    <Text
+                      fontSize="xs"
+                      _light={{
+                        color: "#6A994E",
+                      }}
+                      _dark={{
+                        color: "tertiary.500",
+                      }}
+                      fontWeight="500"
+                      ml="-0.5"
+                      mt="-1"
+                      mr="5"
+                    >
+                      {item.location}{" "}
+                      <FontAwesomeIcon color="#B6B6B6" icon={faLocationDot} />
+                    </Text>
+                    <Text fontSize="xs">{item.user}</Text>
+                  </Stack>
+                  <Text fontWeight="400">{item.description}</Text>
+                </Stack>
+              </Box>
+            </Pressable>
+          ))
         ) : (
           <HStack justifyContent="center" alignItems="center">
             <ActivityIndicator
