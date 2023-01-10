77bf24759d7470bb253abc7e8903269fac7246ae
Max Andersson <maxbjornkga@gmail.com>
2023-01-10 13:42:04 +0100

    error meddelande vid fel inlogg

--- components/pageComponents/Login.js
+++ components/pageComponents/Login.js
@@ -4,17 +4,23 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import { LoginSignupValidationSchema } from "../schemas/LoginSignupValidationSchema";
 import { Formik } from "formik";
-import { Pressable } from "react-native";
-import { Input } from "native-base";
+import { Pressable, Animated } from "react-native";
+import { Input, Alert, VStack, HStack, IconButton, CloseIcon } from "native-base";
 import PrimaryButton from "../inputs/PrimaryButton.js";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React from "react";
+
+
 
 function Login() {
   const [show, setShow] = useState(false);
   const navigation = useNavigation();
   const auth = getAuth();
+  const [errorCode, setErrorCode ] = useState(false)
+  const [errorEmail, setErrorEmail ] = useState(false)
+  const [errorPassword, setErrorPassword ] = useState(false)
 
   function signIn({ email, password }) {
     signInWithEmailAndPassword(auth, email, password)
@@ -24,19 +30,36 @@ function Login() {
           user: auth.currentUser,
         });
       })
-      .catch((error) => {
-        if (error.code === 'auth/user-not-found') {
+      .catch((errors) => {
+        if (errors.code === 'auth/user-not-found') {
+          setErrorCode(true)
           console.log(" adressen existerar inte");
         }
-        if (error.code === 'auth/invalid-email') {
-          console.log("Email adressen existerar inte");
+        if (errors.email === 'auth/invalid-email') {
+          setErrorEmail(true)        
+      
+          console.log(" email existerar inte");
         }
-        if (error.code === 'auth/wrong-password') {
+        if (errors.password === 'auth/wrong-password') {
+          setErrorPassword(true)
             console.log("Lösenordet existerar inte");
         }
       });
   }
+  const animatedValue = React.useRef(new Animated.Value(0)).current;
+  const animated = () => {
+    Animated.timing(animatedValue,{
+     toValue: 0,
+      duration: 2000,
+      useNativeDriver: true
+     
+    }).start();
+  };
+function myFunction(){
+  setErrorCode(false)
 
+}
+ 
   // // TO BE REMOVED
   // onAuthStateChanged(auth, (user) => {
   //   if (user) {
@@ -47,8 +70,9 @@ function Login() {
   //   }
   // });
 
-  return (
+  return (      
     <View style={styles.container}>
+     
       <Image
         style={[
           styles.greenBubble,
@@ -57,7 +81,24 @@ function Login() {
           },
         ]}
         source={require("../../assets/Ellipse.png")}
+        
       />
+           {errorCode && (
+                         <Alert animatedValue={animated} w="50%" borderBottomRadius="2xl" position="absolute" top="0" backgroundColor="danger.400"  >
+                         <VStack space={2} flexShrink={1} w="100%" alignItems="center"  >
+                           <HStack flexShrink={1} space={2} justifyContent="space-between">
+                             <HStack space={2} flexShrink={1}>
+                               <Alert.Icon mt="1" color="black" />
+                               <Text fontSize="md" color="coolGray.800">
+                                 Email eller lösenord fel
+                               </Text>
+                             </HStack>
+                           </HStack>
+                         </VStack>
+                         </Alert> 
+                  )}
+
+         
       <Text style={styles.loginText}>Logga in</Text>
 
       <View style={styles.forms}>
@@ -71,7 +112,6 @@ function Login() {
           onSubmit={(values) => {
             signIn(values) 
           }}
-          
         >
           {({
             handleChange,
@@ -94,6 +134,7 @@ function Login() {
                     Email
                   </Text>
                   <Input
+                    onChange = {myFunction}
                     variant="underlined"
                     placeholder="Email"
                     style={styles.editForm}
@@ -101,18 +142,7 @@ function Login() {
                     onBlur={handleBlur("email")}
                     value={values.email}
                   />
-                  {errors.email && (
-                    <Text
-                      style={{
-                        position: "absolute",
-                        fontSize: 12,
-                        color: "red",
-                        top: 62,
-                      }}
-                    >
-                      {errors.email}
-                    </Text>
-                  )}
+            
                 </View>
                 <View style={styles.form}>
                   <Text
@@ -126,6 +156,7 @@ function Login() {
                   </Text>
                   <View style={styles.passwordCont}>
                     <Input
+                      onChange = {myFunction}
                       style={styles.editForm}
                       variant="underlined"
                       type={show ? "text" : "password"}
