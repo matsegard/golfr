99bedc0ccd60786856a82a86ac61382b6e1b27d2
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-12 10:59:34 +0100

    styling

--- components/HelpPageComponent/HelpInfo.js
+++ components/HelpPageComponent/HelpInfo.js
@@ -1,15 +1,4 @@
-import {
-  Menu,
-  Button,
-  VStack,
-  Select,
-  CheckIcon,
-  Center,
-  NativeBaseProvider,
-  View,
-  Text,
-  Pressable,
-} from "native-base";
+import { VStack, ScrollView, View, Text, Pressable } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -20,6 +9,7 @@ import PrimaryButton from "../inputs/PrimaryButton";
 function HelpInfo() {
   const navigation = useNavigation();
   const [changeArrow, setChangeArrow] = useState(false);
+  const [openQuestion, setOpenQuestion] = useState(false);
   // const [changeArrow1, setChangeArrow1] = useState(true);
   // const [changeArrow2, setChangeArrow2] = useState(true);
   const [shouldOverlapWithTrigger] = React.useState(false);
@@ -45,99 +35,145 @@ function HelpInfo() {
   const handleOpenQuestion = (id) => {
     const item = QnA?.map((qa) => {
       if (qa?.id === id) {
-        setChangeArrow(!changeArrow);
+        setOpenQuestion(!openQuestion);
       }
       return item;
     });
   };
 
   return (
-    <View height="100%">
-      <Text
-        style={{
-          fontSize: 20,
-          fontFamily: "Montserrat",
-          fontWeight: "bold",
-
-          marginTop: 10,
-          marginBottom: 15,
-        }}
-        marginX="auto"
-      >
-        Hjälpcenter
-      </Text>
-      {QnA.map((c) => (
-        <VStack
-          key={c.id}
-          alignSelf="flex-start"
-          w="100%"
-          margin="2"
-          marginBottom="10"
+    <ScrollView vertical showsVerticalScrollIndicator={false} height="100%">
+      <View marginBottom="150">
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "MontserratBold",
+            fontWeight: "bold",
+            marginTop: 50,
+            marginBottom: 55,
+          }}
+          marginX="auto"
         >
-          <Pressable
-            style={{
-              backgroundColor: "#EAEAEA",
-              height: 70,
-              width: "80%",
-              alignSelf: "center",
-              borderRadius: "10",
-            }}
-            onPress={() => handleOpenQuestion(c.id)}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-              marginY="auto"
-            >
-              <Text
+          Hjälpcenter
+        </Text>
+        {QnA.map((c) => (
+          <>
+            {!openQuestion ? (
+              <VStack
                 style={{
-                  fontFamily: "MontserratBold",
-                  fontWeight: "bold",
-                  fontSize: 20,
-                  color: "#6E6E6E",
-                  letterSpacing: "-1%",
+                  margin: 1,
+                  marginBottom: 50,
                 }}
+                width="100%"
+                key={c.id}
+                alignSelf="flex-start"
               >
-                {c.question}
-              </Text>
-              <View justifyContent="end">
-                {!changeArrow ? (
-                  <FontAwesomeIcon
-                    backgroundColor="black"
-                    icon={faChevronDown}
-                    color="#6E6E6E"
-                    size={22}
-                  />
-                ) : (
-                  <>
+                <Pressable
+                  style={{
+                    backgroundColor: "#EAEAEA",
+                    height: 70,
+                    width: "80%",
+                    alignSelf: "center",
+                    borderRadius: "10",
+                  }}
+                  onPress={() => handleOpenQuestion(c.id)}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-around",
+                    }}
+                    marginY="auto"
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "MontserratBold",
+                        fontWeight: "bold",
+                        fontSize: 20,
+                        color: "#6E6E6E",
+                        letterSpacing: "-1%",
+                      }}
+                    >
+                      {c.question}
+                    </Text>
+                    <FontAwesomeIcon
+                      backgroundColor="black"
+                      icon={faChevronDown}
+                      color="#6E6E6E"
+                      size={22}
+                    />
+                  </View>
+                </Pressable>
+              </VStack>
+            ) : (
+              <VStack
+                style={{
+                  margin: 1,
+                  marginBottom: 50,
+                }}
+                width="100%"
+                key={c.id}
+                alignSelf="flex-start"
+              >
+                <Pressable
+                  style={{
+                    backgroundColor: "#EAEAEA",
+                    height: 150,
+                    width: "80%",
+                    alignSelf: "center",
+                    borderRadius: "10",
+                  }}
+                  onPress={() => handleOpenQuestion(c.id)}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-around",
+                      marginTop: 26,
+                    }}
+                    marginY="auto"
+                  >
+                    <Text
+                      style={{
+                        fontFamily: "MontserratBold",
+                        fontWeight: "bold",
+                        fontSize: 20,
+                        color: "#6E6E6E",
+                        letterSpacing: "-1%",
+                      }}
+                    >
+                      {c.question}
+                    </Text>
+
                     <FontAwesomeIcon
                       icon={faChevronUp}
                       color="#6E6E6E"
                       size={22}
                     />
-                    <View
-                      width="90%"
-                      shouldOverlapWithTrigger={shouldOverlapWithTrigger}
-                    >
-                      <Text>{c.answer}</Text>
-                    </View>
-                  </>
-                )}
-              </View>
-            </View>
-          </Pressable>
-        </VStack>
-      ))}
-      <View marginX="auto" marginTop="5" alignItems="center">
-        <Text marginBottom="5">Hittade du inte det du letade efter?</Text>
-        <PrimaryButton
-          label="Kontakta oss"
-          onPress={() => navigation.navigate("Kontakt")}
-        />
+                  </View>
+                  <View
+                    style={{
+                      bottom: 52,
+                    }}
+                  >
+                    <Text textAlign="center">{c.answer}</Text>
+                  </View>
+                </Pressable>
+              </VStack>
+            )}
+          </>
+        ))}
+        <View marginX="auto" marginTop="10" alignItems="center">
+          <Text marginBottom="5" fontFamily="MontserratRegular">
+            Hittade du inte det du letade efter?
+          </Text>
+          <PrimaryButton
+            label="Kontakta oss"
+            onPress={() => navigation.navigate("Kontakt")}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
