1b1eba66847b7892fdd28911f77c25262adf9d61
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-20 14:52:28 +0100

    all questions and styling on QnA

--- components/HelpPageComponent/HelpInfo.js
+++ components/HelpPageComponent/HelpInfo.js
@@ -10,6 +10,7 @@ function HelpInfo() {
   const [openQuestion, setOpenQuestion] = useState(false);
   const [openQuestion2, setOpenQuestion2] = useState(false);
   const [openQuestion3, setOpenQuestion3] = useState(false);
+  const [openQuestion4, setOpenQuestion4] = useState(false);
 
   return (
     <ScrollView vertical showsVerticalScrollIndicator={false} height="100%">
@@ -26,6 +27,119 @@ function HelpInfo() {
         >
           Hjälpcenter
         </Text>
+
+        {!openQuestion4 ? (
+          <VStack
+            style={{
+              margin: 1,
+              marginBottom: 50,
+            }}
+            width="100%"
+            alignSelf="flex-start"
+          >
+            <Pressable
+              style={{
+                backgroundColor: "#EAEAEA",
+                height: 70,
+                width: "80%",
+                alignSelf: "center",
+                borderRadius: "10",
+              }}
+              onPress={() => setOpenQuestion4(!openQuestion4)}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                }}
+                marginY="auto"
+              >
+                <Text
+                  style={{
+                    fontFamily: "MontserratBold",
+                    fontWeight: "bold",
+                    fontSize: 19,
+                    color: "#6E6E6E",
+                    letterSpacing: "-1%",
+                    marginLeft: 20,
+                  }}
+                >
+                  Vad är Golfr?
+                </Text>
+                <FontAwesomeIcon
+                  style={{
+                    marginRight: 20,
+                  }}
+                  backgroundColor="black"
+                  icon={faChevronDown}
+                  color="#6E6E6E"
+                  size={22}
+                />
+              </View>
+            </Pressable>
+          </VStack>
+        ) : (
+          <VStack
+            style={{
+              margin: 1,
+              marginBottom: 50,
+            }}
+            width="100%"
+            alignSelf="flex-start"
+          >
+            <Pressable
+              style={{
+                backgroundColor: "#EAEAEA",
+                height: 136,
+                width: "80%",
+                alignSelf: "center",
+                borderRadius: "10",
+              }}
+              onPress={() => setOpenQuestion4(!openQuestion4)}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginTop: 26,
+                }}
+                marginY="auto"
+              >
+                <Text
+                  style={{
+                    fontFamily: "MontserratBold",
+                    fontWeight: "bold",
+                    fontSize: 19,
+                    color: "#6E6E6E",
+                    letterSpacing: "-1%",
+                    marginLeft: 20,
+                  }}
+                >
+                  Vad är Golfr?
+                </Text>
+
+                <FontAwesomeIcon
+                  style={{
+                    marginRight: 20,
+                  }}
+                  icon={faChevronUp}
+                  color="#6E6E6E"
+                  size={22}
+                />
+              </View>
+              <View
+                style={{
+                  bottom: 4,
+                }}
+              >
+                <Text textAlign="center" padding="3">
+                  Golfr är en applikation där man som användare kan hyra och
+                  hyra ut sina golfklubbor och övriga golfprylar.
+                </Text>
+              </View>
+            </Pressable>
+          </VStack>
+        )}
 
         {!openQuestion ? (
           <VStack
@@ -155,6 +269,7 @@ function HelpInfo() {
                 style={{
                   flexDirection: "row",
                   justifyContent: "space-around",
+                  alignItems: "center",
                 }}
                 marginY="auto"
               >
@@ -165,12 +280,16 @@ function HelpInfo() {
                     fontSize: 19,
                     color: "#6E6E6E",
                     letterSpacing: "-1%",
-                    width: 220,
+                    width: 260,
+                    marginLeft: 17,
                   }}
                 >
                   Hur fungerar kontakten när jag ska hyra en produkt?
                 </Text>
                 <FontAwesomeIcon
+                  style={{
+                    marginRight: 10,
+                  }}
                   backgroundColor="black"
                   icon={faChevronDown}
                   color="#6E6E6E"
@@ -191,7 +310,7 @@ function HelpInfo() {
             <Pressable
               style={{
                 backgroundColor: "#EAEAEA",
-                height: 225,
+                height: 195,
                 width: "80%",
                 alignSelf: "center",
                 borderRadius: "10",
@@ -202,7 +321,6 @@ function HelpInfo() {
                 style={{
                   flexDirection: "row",
                   justifyContent: "space-around",
-                  marginTop: 26,
                 }}
                 marginY="auto"
               >
@@ -213,13 +331,21 @@ function HelpInfo() {
                     fontSize: 19,
                     color: "#6E6E6E",
                     letterSpacing: "-1%",
-                    width: 220,
+                    width: 260,
+                    marginLeft: 17,
                   }}
                 >
                   Hur fungerar kontakten när jag ska hyra en produkt?
                 </Text>
-
-                <FontAwesomeIcon icon={faChevronUp} color="#6E6E6E" size={22} />
+                <FontAwesomeIcon
+                  style={{
+                    marginRight: 9,
+                    marginTop: 15,
+                  }}
+                  icon={faChevronUp}
+                  color="#6E6E6E"
+                  size={22}
+                />
               </View>
               <View
                 style={{
@@ -272,9 +398,12 @@ function HelpInfo() {
                     letterSpacing: "-1%",
                   }}
                 >
-                  FRÅGA
+                  Hur fungerar betalningen?
                 </Text>
                 <FontAwesomeIcon
+                  style={{
+                    marginRight: 3,
+                  }}
                   backgroundColor="black"
                   icon={faChevronDown}
                   color="#6E6E6E"
@@ -295,7 +424,7 @@ function HelpInfo() {
             <Pressable
               style={{
                 backgroundColor: "#EAEAEA",
-                height: 150,
+                height: 122,
                 width: "80%",
                 alignSelf: "center",
                 borderRadius: "10",
@@ -319,25 +448,33 @@ function HelpInfo() {
                     letterSpacing: "-1%",
                   }}
                 >
-                  FRÅGA
+                  Hur fungerar betalningen?
                 </Text>
 
-                <FontAwesomeIcon icon={faChevronUp} color="#6E6E6E" size={22} />
+                <FontAwesomeIcon
+                  style={{
+                    marginRight: 3,
+                  }}
+                  icon={faChevronUp}
+                  color="#6E6E6E"
+                  size={22}
+                />
               </View>
               <View
                 style={{
-                  bottom: 52,
+                  bottom: 4,
                 }}
               >
                 <Text textAlign="center" padding="3">
-                  SVAR
+                  Betalningsmetoden bestämmer användarna själva! Och detta sker
+                  utanför Golfrs ansvar.
                 </Text>
               </View>
             </Pressable>
           </VStack>
         )}
 
-        <View marginX="auto" marginTop="10" alignItems="center">
+        <View marginX="auto" marginTop="1" alignItems="center">
           <Text marginBottom="5" fontFamily="MontserratRegular">
             Hittade du inte det du letade efter?
           </Text>
