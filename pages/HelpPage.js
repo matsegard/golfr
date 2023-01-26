1fb353ac184faf7de80eb279e9cb118ea9eb6942
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-26 11:25:04 +0100

    fixed errors

--- pages/HelpPage.js
+++ pages/HelpPage.js
@@ -1,10 +1,7 @@
 import { View } from "native-base";
-
-import HelpInfo from "../components/HelpPageComponent/HelpInfo.js";
+import HelpInfo from "../components/helpPageComponent/HelpInfo.js";
 
 import Navbar from "../components/bars/Navbar";
-
-
 
 function HelpPage() {
   return (
