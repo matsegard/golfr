8a4044afe6cf73d8392a902185214b3d06464608
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2023-01-26 11:31:27 +0100

    fixed loading spinner and text bug

--- pages/HelpPage.js
+++ pages/HelpPage.js
@@ -1,8 +1,6 @@
 import { View } from "native-base";
 import HelpInfo from "../components/helpPageComponent/HelpInfo.js";
 
-import Navbar from "../components/bars/Navbar";
-
 function HelpPage() {
   return (
     <View>
